cb83672fb4626a87f64a920879c3075cd2231510
Robson Costa <robsoncosta@pjc.mt.gov.br>
2024-03-28 10:08:17 -0400

    cria projeto

---
+++ src/main/java/br/gov/mt/sesp/sespprojetopraticospring/SespProjetoPraticoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mt.sesp.sespprojetopraticospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SespProjetoPraticoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SespProjetoPraticoSpringApplication.class, args);
+    }
+
+}
